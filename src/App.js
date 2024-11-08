0eca8dd7ae838a965e5c8318cb24794614b992b5
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-11-08 12:02:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Survey from './Survey';  // Import the Survey component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <Survey />  {/* Render the Survey component */}
-      </header>
+
     </div>
   );
 }
